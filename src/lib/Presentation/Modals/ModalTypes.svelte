4f6f17880160731660949d3cd45d2a0b1ec63ffc
Neil Savin <savin.neil.56@gmail.com>
2025-02-18 16:43:06 +0100

    Add modal types

---
+++ src/lib/Presentation/Modals/ModalTypes.svelte
@@ -0,0 +1,19 @@
+<script>
+	import CodeSnippet from '$lib/Presentation/PresentationAtoms/CodeSnippet.svelte';
+	import InlineCode from '$lib/Presentation/PresentationAtoms/InlineCode.svelte';
+</script>
+
+<h2>ModalElement</h2>
+<p>
+	The type of element you get when using <InlineCode code={'bind:this'} /> on a Modal component.
+</p>
+<CodeSnippet lang="typescript" code={`import type { ModalElement } from '@nelhoa/svelte-modals'`} />
+
+<h2>ModalProps</h2>
+<!-- <p>More about props in the <a href="/docs/tooltips-props">dedicated section</a>.</p> -->
+
+<CodeSnippet lang="typescript" code={`import type { ModalProps } from '@nelhoa/svelte-modals'`} />
+<h2>ModalRemote</h2>
+<p>Remote attached to a Modal component.</p>
+
+<CodeSnippet lang="typescript" code={`import type { ModalRemote } from '@nelhoa/svelte-modals'`} />
--- src/lib/Presentation/index.ts
+++ src/lib/Presentation/index.ts
@@ -5,6 +5,7 @@ import ModalOpenVariant from './Introduction/ModalOpenVariant.svelte';
 import Anchor from './Introduction/Anchor.svelte';
 import TooltipIntro from './Tooltips/TooltipProps.svelte';
 import TooltipTypes from './Tooltips/TooltipTypes.svelte';
+import ModalTypes from './Modals/ModalTypes.svelte';
 
 export type title = { title: string; items: presentationItem[] };
 export type presentationItem = { subtitle: string; slug: string; component: Component };
@@ -37,6 +38,13 @@ export const index: title[] = [
 		items: [
 			{ subtitle: `Props`, slug: 'tooltips-props', component: TooltipIntro },
 			{ subtitle: 'Typescript', slug: 'tooltips-types', component: TooltipTypes }
+		]
+	},
+	{
+		title: 'Modal',
+		items: [
+			// { subtitle: `Props`, slug: 'tooltips-props', component: TooltipIntro },
+			{ subtitle: 'Typescript', slug: 'modal-types', component: ModalTypes }
 		]
 	}
 ];
