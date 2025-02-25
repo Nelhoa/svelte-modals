1a7d9af0d734f4bc39cdd3e616965402cf2bb54d
Neil Savin <savin.neil.56@gmail.com>
2025-02-25 14:27:31 +0100

    Correct shallow routing

--- src/lib/Presentation/Modals/ShallowRouting.svelte
+++ src/lib/Presentation/Modals/ShallowRouting.svelte
@@ -4,16 +4,16 @@
 	import Modal from '$lib/Components/Modal/Modal.svelte';
 </script>
 
-<!-- <p>Need further dev.</p> -->
+<p>Need further dev.</p>
 
 <!-- <button class="">
 	My button
 	<Modal shallow={{ pushState, stateName: 'openModal', page: page }}>Modal content</Modal>
 </button> -->
 
-<button
+<!-- <button
 	class="flex bg-white border text-black/95 font-atma font-semibold tracking-widest text-[20px] gap-x-2 focus:outline-none"
 >
 	<img src="/svelte-pops-icon.svg" width="30" alt="" class="pb-[2px]" /> svelte-pops
 	<Modal class="font-semibold">Popover & Tooltips</Modal>
-</button>
+</button> -->
